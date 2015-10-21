52e22027801abc7149a13c249954ba8d074625a4
Jadson Santos <jadsonjs@gmail.com>
2015-10-21 16:17:57 -0300

    Update V1__Initial_Version.sql

--- Database/db/migration/V1__Initial_Version.sql
+++ Database/db/migration/V1__Initial_Version.sql
@@ -1,3 +1,3 @@
-insert into person (name) values('Jose')
-insert into person (name) values('Joao')
-insert into person (name) values('Paulo')
+insert into person (name) values('Jose');
+insert into person (name) values('Joao');
+insert into person (name) values('Paulo');
