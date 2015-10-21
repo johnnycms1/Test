d4237cad3492b37585681f8d3dd30649abaef674
Jadson Santos <jadson@info.ufrn.br>
2015-10-21 16:12:45 -0300

    232343 - testes
    Signed-off-by: Jadson Santos <jadson@info.ufrn.br>

--- Project/src/main/java/org/jadson/Person.java
+++ Project/src/main/java/org/jadson/Person.java
@@ -2,6 +2,15 @@ package org.jadson;
 
 import org.apache.commons.collections.list.GrowthList;
 
+/**
+ * 
+ * @author jadson
+ *
+ * <p><i>This program is distributed without any warranty and it
+ * can be freely redistributed for research, classes or private studies, 
+ * since the copyright notices are not removed.</i></p>
+ *
+ */
 public class Person {
     private final String name;
 
