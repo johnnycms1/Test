686cc4835e9ebd7f72bfa33f659f5ac7ddc7594b
Johnny Marçal <johnnycms@gmail.com>
2015-12-14 01:00:46 -0300

    commit and push
    Conflicts:
    	Database/db/migration/V1__Initial_Version.sql

--- Database/db/migration/V1__Initial_Version.sql
+++ Database/db/migration/V1__Initial_Version.sql
@@ -1,3 +1,5 @@
 insert into person (name) values('Jose');
 insert into person (name) values('Joao');
 insert into person (name) values('Paulo');
+insert into person (name) values('Johnny');
+insert into person (name) values('Angela');
