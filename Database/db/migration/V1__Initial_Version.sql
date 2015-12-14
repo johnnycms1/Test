2a142e270fd3fe029b8894ac8a90ecf2b0963359
Johnny Marçal <johnnycms@gmail.com>
2015-12-14 01:00:46 -0300

    commit and push
    Signed-off-by: Johnny Marçal <johnnycms@gmail.com>

--- Database/db/migration/V1__Initial_Version.sql
+++ Database/db/migration/V1__Initial_Version.sql
@@ -2,3 +2,4 @@ insert into person (name) values('Jose');
 insert into person (name) values('Joao');
 insert into person (name) values('Paulo');
 insert into person (name) values('Johnny');
+insert into person (name) values('Angela');
