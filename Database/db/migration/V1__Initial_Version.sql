a3ce6b296c9a1aa7bb0dc618791987f153b7da9a
Johnny Marçal <johnnycms@gmail.com>
2015-12-14 01:17:06 -0300

    Signed-off-by: Johnny Marçal <johnnycms@gmail.com>

--- Database/db/migration/V1__Initial_Version.sql
+++ Database/db/migration/V1__Initial_Version.sql
@@ -3,9 +3,6 @@ insert into person (name) values('Joao');
 insert into person (name) values('Paulo');
 insert into person (name) values('Johnny');
 insert into person (name) values('Angela');
-<<<<<<< HEAD
 insert into person (name) values('Carla');
 insert into person (name) values('Bruna');
-=======
 insert into person (name) values('Carla');
->>>>>>> refs/heads/master
